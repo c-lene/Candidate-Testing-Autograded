1e89c4e482d35a94d1bb56d79ee273d8e308b5aa
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-06-25 01:13:43 -0500

    completed & tested Task 2 - Canditate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,6 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -11,9 +12,15 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -24,18 +31,33 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i])); 
+  }
+
+  //candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {                      // Add "\\" OR statement  or "else if" statement to accept LOWER Case answer - CT
+  
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`${questions[i]} 
+    Your answer was ${candidateAnswers[i]}. 
+    The correct answer is ${correctAnswers[i]}.
+    `);
+  }
+
+  
+
+  
+  /*if (candidateAnswer === correctAnswer) {                      // This was code from 'Task 1' - CT
     console.log("Good Job! Your answer is CORRECT!");
   } else {
     console.log("Sorry, your answer was INCORRECT. The correct answer is " + correctAnswer + ".");
-  }
+  }*/
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
